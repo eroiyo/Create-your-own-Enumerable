e12430388d4b47b05420295c24698dde42f84518
eroiyo <59938389+eroiyo@users.noreply.github.com>
2021-10-07 16:09:19 -0400

    enumerable

---
+++ my_enumerable.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+# a extension for the list object, to be able to make use of it's each function
+module MyEnumerable
+  def all?
+    each do |e|
+      return false unless yield e
+    end
+    true
+  end
+
+  def any?
+    each do |e|
+      return true if yield e
+    end
+    false
+  end
+
+  def max?
+    max = nil
+    each do |e|
+      max = e if max.nil?
+      max = e if max < e
+    end
+    max
+  end
+
+  def filter
+    filtered = []
+    each { |e| filtered.push(e) if yield e }
+    filtered
+  end
+
+  def min?
+    min = nil
+    each do |e|
+      min = e if min.nil?
+      min = e if min > e
+    end
+    min
+  end
+end
